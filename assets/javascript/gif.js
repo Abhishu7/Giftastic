348c2694bf56128db3872e88c731e6488f06e1fd
Abhishu Mukherjee <abhishu.m@utexas.edu>
2019-09-17 16:39:27 -0500

    adding buttons function

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,8 +1,29 @@
 $(document).ready(function () {
-    var animals = ["elephant", "hippopotamus", "tiger", "giraffe", "rhinoceros"];
-    
+    var animals = ["elephant", "hippopotamus", "giraffe", "tiger", "lion", "rhinoceros", "cheetah", "zebra"]
+  
     function renderButtons() {
-        $("#animalButtons").empty();
+      $("#animalButtons").empty();
+  
+      for (var i = 0; i < animals.length; i++) {
+  
+        var a = $("<button>");
+  
+        a.addClass("animalBtn");
+        a.attr("data-name", animals[i]);
+        a.text(animals[i]);
+  
+        $("#animalButtons").append(a);
+    } 
+} 
+$("#addAnimal").on("click", function (event) {
+  event.preventDefault();
+  var animal = $("#animalInput").val().trim();
 
-    }
+  if (($.inArray(animal, animals) < 0) && (animal.length > 0)) {
+    animals.push(animal);
+  }
+  renderButtons();
+  $("#animalInput").val("");
+});
+ 
 })
