dcba69f156933fe631ae72077203bff2e6c8af61
Abhishu Mukherjee <abhishu.m@utexas.edu>
2019-09-17 16:02:50 -0500

    updating HTML

---
+++ assets/javascript/gif.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var animals = ["elephant", "hippopotamus", "tiger", "giraffe", "rhinoceros"];
+    
+    function renderButtons() {
+        $("#animalButtons").empty();
+
+    }
+})
