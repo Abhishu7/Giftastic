9a9e921ff2505dcb0025a8060d23e566095a9c35
Abhishu Mukherjee <abhishu.m@utexas.edu>
2019-09-18 17:19:18 -0500

    added giphy URL and displays images

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
   function renderButtons() {
     $("#soccerButtons").empty();
-
+    console.log(players);
     for (var i = 0; i < players.length; i++) {
 
       var a = $("<button>");
@@ -18,14 +18,53 @@ $(document).ready(function () {
   $("#addSoccer").on("click", function (event) {
     event.preventDefault();
     var player = $("#soccerInput").val().trim();
-
+    console.log(player);
     if (($.inArray(player, players) < 0) && (player.length > 0)) {
       players.push(player);
     }
     renderButtons();
     $("#soccerInput").val("");
   });
+  $(document).on("click", ".soccerBtn", function (event) {
+    event.preventDefault();
+    var player = $(this).attr("data-name");
+    $("#soccerGif").empty();
+
+    var queryURL = "https://api.giphy.com/v1/gifs/random?tag=" +
+      player + "&api_key=5AMolnIU0fKHH8CChmVDrbPXDGPVogk8";
+    
+    for (var i = 0; i < 10; i++) {
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function (response) {
+
+          var results = response.data;
+
+          var soccerDiv = $("<div>");
+
+          var soccerImage = $("<img>");
+          console.log(results.images.fixed_height_still.url);
+
+          soccerImage.attr({
+            src: results.images.fixed_height_still.url,
+            dataStill: results.images.fixed_height_still.url,
+            dataAnimate: results.images.fixed_height.url,
+            dataState: "still"
+          });
+          soccerDiv.append(soccerImage);
+
+          $("#soccerGif").prepend(soccerDiv);
+
+        });
+    }
+  });
+
   
+  renderButtons();
 
 });
+
 
